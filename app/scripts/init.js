351688ebd5518bd293f51717cf5633cc492db0c3
Svirydovich Vladislav <v.sviridovich@imedia.by>
2022-05-23 17:43:54 +0300

    animations

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -20,4 +20,81 @@ document.addEventListener('DOMContentLoaded', () => {
   app.modals.init();
 
   new LazyLoad();
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.from('.main-page__grid-content--1', {
+    scrollTrigger: '.main-page__grid-content--1',
+    y: 100,
+    duration: 1,
+    opacity: 0,
+    delay: 0.2,
+  });
+
+  gsap.from('.main-page__grid-img--1', {
+    scrollTrigger: '.main-page__grid-img--1',
+    y: 100,
+    duration: 1,
+    opacity: 0,
+    delay: 0.4,
+  });
+
+  gsap.from('.main-page__grid-img--2', {
+    scrollTrigger: '.main-page__grid-img--2',
+    y: 100,
+    duration: 1,
+    opacity: 0,
+    delay: 0.6,
+  });
+
+  gsap.from('.main-page__grid-content--2', {
+    scrollTrigger: '.main-page__grid-content--2',
+    y: 100,
+    duration: 1,
+    opacity: 0,
+    delay: 0.8,
+  });
+
+  gsap.from('.blog-article', {
+    scrollTrigger: '.blog-article',
+    y: 50,
+    duration: 1,
+    opacity: 0,
+    delay: 0.2,
+    stagger: 0.2,
+  });
+
+  gsap.from('.our-projects-page__projects-item', {
+    scrollTrigger: '.our-projects-page__projects-item',
+    y: 100,
+    duration: 1,
+    opacity: 0,
+    delay: 0.2,
+    stagger: 0.5,
+  });
+
+  const showAnim = gsap.from('.header', {
+    yPercent: -100,
+    paused: true,
+    duration: 0.2,
+  }).progress(1);
+
+  ScrollTrigger.create({
+    start: 'top top',
+    end: 99999,
+    onUpdate: (self) => {
+      self.direction === -1 ? showAnim.play() : showAnim.reverse()
+    },
+  });
+
+  const items = document.querySelectorAll('.catalog-page__catalog-item');
+
+  items.forEach((item) => {
+    ScrollTrigger.create({
+      trigger: item,
+      markers: true,
+      start: '-40% top',
+      toggleClass: 'is-active',
+    });
+  });
 });
